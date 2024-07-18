6975864763acd9298485bdc8c3101beeec3a3e3a
exewen <exeweb@163.com>
2024-07-18 15:31:00 +0800

    1

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -3,9 +3,7 @@
 namespace Exewen\Cdiscount\Services;
 
 use Exewen\Config\Contract\ConfigInterface;
-use Exewen\Di\Container;
 use Exewen\Http\Contract\HttpClientInterface;
-use Exewen\Logger\Contract\LoggerInterface;
 
 class AuthService
 {
@@ -13,19 +11,19 @@ class AuthService
     private $driver;
     private $authUrl = '/auth/realms/maas/protocol/openid-connect/token';
 
-    public function __construct(ConfigInterface $config)
+    public function __construct(HttpClientInterface $httpClient, ConfigInterface $config)
     {
-        $this->httpClient = Container::getInstance()->get(HttpClientInterface::class);
-        $this->driver = $config->get('cdiscount.channel_auth');
+        $this->httpClient = $httpClient;
+        $this->driver     = $config->get('cdiscount.channel_auth');
     }
 
     public function getAuth(string $clientId, string $clientSecret)
     {
         return $this->httpClient->post($this->driver, $this->authUrl, [
-            'grant_type' => 'client_credentials',
-            'client_id' => $clientId,
+            'grant_type'    => 'client_credentials',
+            'client_id'     => $clientId,
             'client_secret' => $clientSecret,
-        ]);
+        ], [], [], 'form_params');
     }
 
 
